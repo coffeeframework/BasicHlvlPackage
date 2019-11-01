320947d03c0914bf4a81b39c8732e97ba8691483
RigobertoBolanos <RigobertoBolanos@users.noreply.github.com>
2019-11-01 09:41:17 -0500

    Re-structuration
    Only the classes concerning the hlvl basic package were kept in order to become a self repository

---
+++ basickhlvlpackage/src/main/java/com/github/coffeeframework/basickhlvlpackage/DecompositionType.java
@@ -0,0 +1,13 @@
+package com.github.coffeeframework.basickhlvlpackage;
+
+/**
+ * Enumeration declaring the types of decompostion
+ * in the basic dialect of HLVL (Hlvl(basic))
+ * @author Angela Villota
+ * Coffee V1
+ * January 2019
+ */
+public enum DecompositionType {
+	Mandatory,
+	Optional
+}
